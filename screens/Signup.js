34f7d43a294fd6a8dd3c5f531fb68807d6754953
André Luís Batista Kunde <andrelbkunde@gmail.com>
2020-01-28 19:12:50 -0300

    Begining screens

---
+++ screens/Signup.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+
+class Signup extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Signup Screen</Text>
+      </View>
+    )
+  }
+}
+
+export default Signup
