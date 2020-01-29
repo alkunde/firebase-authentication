e10cf112d3298a60c1faf70bee46a5e04360670a
André Luís Batista Kunde <andrelbkunde@gmail.com>
2020-01-29 07:28:28 -0300

    Including redux

--- App.js
+++ App.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunkMiddleware from 'redux-thunk'
 
 import SwitchNavigator from './navigation/SwitchNavigator';
+import reducer from './reducers'
+
+const middleware = applyMiddleware(thunkMiddleware);
+const store = createStore(reducer, middleware);
 
 export default class App extends Component {
   render() {
-    return <SwitchNavigator />
+    return (
+      <Provider store={store}>
+        <SwitchNavigator />
+      </Provider>
+    )
   }
 }
